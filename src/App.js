5ab0d33225701d47bf8f8fd8398c0d03fb7ad56e
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-12-11 00:51:41 +0900

    #8.2 Router Setup part One

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/food"><h1>Home</h1></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
