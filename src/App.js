5b378ac980dd492125a86aa7375693c8e84ddd9f
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-12-14 15:27:51 +0900

    #8.5 Auth POC

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./screens/home";
 import Login from "./screens/Login";
+// import NotFound from "./screens/NotFound";
 
 function App() {
-  const isLoggedIn = false;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+          <Route path="/" element={isLoggedIn ? <Home setIsLoggedIn={setIsLoggedIn} /> : <Login setIsLoggedIn={setIsLoggedIn} />} />
           <Route
             path="/nomad-coders"
             element={
