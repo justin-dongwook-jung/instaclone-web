eae0000e7709b8b43b2df1dbb09e8e3d72ea6dac
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-12-14 15:10:43 +0900

    #8.4 Router Setup part Two

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./screens/home";
+import Login from "./screens/Login";
 
 function App() {
+  const isLoggedIn = false;
+
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/food"><h1>Home</h1></Route>
+          <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+          <Route
+            path="/nomad-coders"
+            element={
+              <div>
+                <h1>Nomad Coders</h1>
+                <h1>{!isLoggedIn ? "Please Log in..." : null}</h1>
+              </div>
+            }
+          />
         </Routes>
       </Router>
     </div>
